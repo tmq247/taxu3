5317b7725965c62c83e3cc20a59feb73146eb59f
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-11-25 03:00:25 +0700

    Update luna.py

--- luna.py
+++ luna.py
@@ -235,9 +235,9 @@ async def check_balance(_, message):
 @bot.on_message(filters.command("tx"))
 async def start_taixiu(_, message):
 	#await game_timer()
-	timer_thread.start()
-	timer_thread = threading.Thread(target=game_timer)
 	#timer_thread.start()
+	timer_thread = threading.Thread(target=game_timer)
+	timer_thread.start()
     #else:
 	    #await bot.send_message(group_chat_id, "test.")
 
