9af60c4b5d850e703178dd70bbeb226179f47d86
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-11-25 02:53:13 +0700

    Update luna.py

--- luna.py
+++ luna.py
@@ -174,7 +174,7 @@ async def game_timer():
 	await bot.send_message(group_chat_id, "Bắt đầu cược! Có 45s để đặt cược.")
 	time.sleep(20)  # Wait for 120 seconds
 	await bot.send_message(group_chat_id, "Hết thời gian cược. Kết quả sẽ được công bố ngay sau đây.")
-	await start_game()
+	start_game()
 	
 
 # Function to handle user messages
