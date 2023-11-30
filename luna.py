39b5ffafac147a95ffeafdac4a5dc103c35f6803
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-11-30 14:53:06 +0700

    Update luna.py

--- luna.py
+++ luna.py
@@ -164,7 +164,7 @@ def confirm_bet(user_id, bet_type, bet_amount, ten_ncuoc, message):
         bot.send_message(group_chat_id, f"{user_bets}")
         # Check user balance
         if user_balance[user_id] >= bet_amount:
-            user_bets[user_id] = {'T': 0, 'X': 0}  # Initialize the user's bets if not already present
+            
             nchoi = user_bets.get(user_id, [])
             
             if user_id in user_bets:
@@ -176,6 +176,7 @@ def confirm_bet(user_id, bet_type, bet_amount, ten_ncuoc, message):
                     bot.send_message(group_chat_id, f"{user_id} {bet_type} {tongtiencuoc}")
                     user_bets[user_id][bet_type] += tongtiencuoc
             #else:
+            user_bets[user_id] = {'T': 0, 'X': 0}  # Initialize the user's bets if not already present
             user_bets[user_id][bet_type] += bet_amount
             user_balance[user_id] -= bet_amount
             
