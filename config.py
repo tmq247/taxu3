f2ddb8cd63e63e02bc6cbc18ef66c3b4df3eeb2b
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-11-30 23:47:42 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-API_KEY = '6784844273:AAGdaEkuudWmwe-PsfYLFXKBzW_TF_pWIDM' '6757521267:AAE5IHnHoESuOPViTNOJsxrYMlit6jtgbwQ'
+#API_KEY = '6784844273:AAGdaEkuudWmwe-PsfYLFXKBzW_TF_pWIDM' '6757521267:AAE5IHnHoESuOPViTNOJsxrYMlit6jtgbwQ'
 group_chat_id = '-1002121532989'
 group_chat_id2 = "-1002121532989"
 admin_user_id = 6337933296, 6630692765, 5838967403
 #6337933296 or 6630692765 or 5838967403
 #list(map(int, os.environ.get('6145782184', '5838967403').split()))
-bot_token = "6037488584:AAF_fjZD_olaLtqe_QmUEdjvjPRAeGqYAfo"
+bot_token = "6982786295:AAGrLzZzUBulmPqH28yikLEovd5xgkWnzj4"
 bot_token2 = "6272942112:AAFvppVJA8l0qS2tfkeCVJa4MIDuEszbTtA"
 api_id=27062094
 api_hash="2edbe92cfa9db035248bfc8957ba1b95"
