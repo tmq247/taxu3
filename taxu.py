de7e3e7a13c77b2f2cbb8936c11e8d6158d97f78
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-12-14 23:48:30 +0700

    Update taxu.py

--- taxu.py
+++ taxu.py
@@ -296,7 +296,8 @@ async def set_balance(_, message):
 async def update_balance(diem, user_id, message):
   load_balance_from_file()
   chat_id = message.chat.id
-  user_ids = await bot.get_users(user_id)
+  user_ids = await bot.get_users(user_id).mention
+    
   try:
     user_input = message.text.split()
     if len(user_input) != 3:
@@ -316,7 +317,7 @@ async def update_balance(diem, user_id, message):
 🫂Số Điểm Hiện Tại: {new_balance:,} điểm🐥
 🐝Chúc Bạn Chơi Game Vui Vẻ🐳
 """ 
-    text = f"""🔥Chúc mừng {user_id.mention} đã bơm máu thành công⚡️⚡️"""
+    text = f"""🔥Chúc mừng {user_id} đã bơm máu thành công⚡️⚡️"""
     await bot.send_message(user_id, notification_message)
     # Gửi thông báo đến nhóm về việc có người chơi đặt cược
     group_chat_id2 = -1001712715936
