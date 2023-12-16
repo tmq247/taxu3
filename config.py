ee1316f4d247eb9da4828c2b2807530339517ff7
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-12-16 19:21:39 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,12 +6,17 @@ admin = 6337933296, 6630692765, 5838967403, 6050066066
 #list(map(int, os.environ.get('6145782184', '5838967403').split()))
 bot_token = "6272942112:AAFvppVJA8l0qS2tfkeCVJa4MIDuEszbTtA"
 bot_token2 = "6037488584:AAF_fjZD_olaLtqe_QmUEdjvjPRAeGqYAfo"
+bot_token3 = "6698135046:AAH7PWSndks__CEgjVZRZHa5FzZUGHf02D4"
 api_id=27062094
 api_hash="2edbe92cfa9db035248bfc8957ba1b95"
 group_id = -1002030087607
 group_id2 = -1002030087607
+group_id3 = -1002030087607
 channel_id = -1002030087607
 tile_thang = 1.95
+admin_id = "6337933296" #coi
+admin_id2 = "6337933296" #bang
+admin_id3 = "6337933296" #na
 # 6272942112:AAFvppVJA8l0qS2tfkeCVJa4MIDuEszbTtA
 # 6037488584:AAF_fjZD_olaLtqe_QmUEdjvjPRAeGqYAfo
 #another_bot_token = "6755926001:AAGD0Gc9xMomJgnfhwjeIENF9XO0reeST1o"
