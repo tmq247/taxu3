f3d99ebcb2aa12a0edf24ef3c3003015026503f0
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-12-18 09:22:33 +0700

    Update luna.py

--- luna.py
+++ luna.py
@@ -73,7 +73,7 @@ def mo_bot():
     if bot_trangthai[user_id] == "bot_diem":
         bot_trangthai[user_id] = ("bot_diem", trangthai)
         with open(bot_FILE, "a") as f:
-            f.write(f"{user_id}:{"bot_diem"}, {trangthai}\n")
+            f.write(f"{user_id}: {"bot_diem"}: {trangthai}\n")
         return user_id
     
 
