7d2eb158925f4b75b998cd96cfc859026d6301a6
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-12-18 09:58:01 +0700

    Update luna.py

--- luna.py
+++ luna.py
@@ -197,6 +197,7 @@ def handle_message(_, message: Message):
     from_user = message.from_user.id
     user_id = Luna.get_users(from_user).id
     grid = chat_id
+    xem_bot()
     if user_id not in bot_trangthai:
         return Luna.send_message(chat_id, "Vui lòng khởi động bot @diemallwin_bot và @alltowin_bot bằng lệnh /start trong ib để chơi game.")
     if len(mo_game) > 0 and mo_game[grid]['tthai'] == 2:
