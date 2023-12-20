a9a9e1a5c003bd792ea9cf1fe20aa766aa3cbbfc
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-12-21 04:17:54 +0700

    Update luna.py

--- luna.py
+++ luna.py
@@ -337,7 +337,7 @@ def confirm_bet(user_id, bet_type, bet_amount, ten_ncuoc, message):
 async def chinh_cau(_, message: Message):
     lenh, cau = message.text.split(" ", 2)
     print(cau)
-    chinhcau = {}
+    chinhcau = {cau}
     if cau == "tai":
         chinhcau = "tai"
     if cau == "xiu":
