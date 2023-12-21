dfea3d1ad2fface1c87f10b2d6a9695951aafa66
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-12-22 03:44:47 +0700

    Update luna.py

--- luna.py
+++ luna.py
@@ -607,7 +607,7 @@ def listdiem(_, message: Message):
     chat_id = message.chat.id
     with open("id.txt", "r") as f:
         a = f.read()
-        for line in a):
+        for line in a:
             user_id, diem = line.strip().split()
             print(user_id)
             print(diem)
