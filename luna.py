db8005b3ad1972ee8808ee0e45f86d5379a76c1e
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-12-22 19:21:31 +0700

    Update luna.py

--- luna.py
+++ luna.py
@@ -629,7 +629,7 @@ def top_diem(_, message: Message):
     chat_id = message.chat.id
     if chat_id == group_id2 or chat_id == group_id3:
           top = f"Top 10 điểm cao nhất:\n"
-          for user_id, diem in user_balance.get(user_id, 0):
+          for diem in user_balance.get(admin, 0):
               td = sorted(user_balance, diem)
               top += f"""{td}\n"""
                     #topdiem[int(user_id)] += (int(diem))
