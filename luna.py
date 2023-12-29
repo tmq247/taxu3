b0e1cc22b25aa2b12a433afb199d102582b63e23
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-12-29 21:53:52 +0700

    Update luna.py

--- luna.py
+++ luna.py
@@ -619,7 +619,7 @@ def listdiem(_, message: Message):
                 balance = int(balance)
             topdiem[int(user_id)] = {"diem": 0}
             topdiem[int(user_id)]["diem"] += balance
-        td = sorted(topdiem, key="diem")
+        td = sorted(topdiem, key=lambda x: x[3]")
         print(td[:10])
     
 
